0037af43f149a8ab8e9f0c08771a7faf0c3d053b
Sridharan Kuppa <sridharan.kuppa@gmail.com>
2014-03-23 17:35:59 -0700

    adding mysql driver and application properties

--- src/main/java/com/rest/RestApplication.java
+++ src/main/java/com/rest/RestApplication.java
@@ -1,10 +1,7 @@
 package com.rest;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,15 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class RestApplication {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(RestApplication.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        SpringApplication.run(RestApplication.class, args);
     }
 
 }
